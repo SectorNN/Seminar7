bd01dd1f8a842786056f4b60128d54c204dd9927
SectorNN <net.laifa@gmail.com>
2022-10-16 20:36:50 +0300

    Добавлено вычисление комплексных чисел

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import view
 import rational
 import logger
+import complex
 
 
 match view.item_sel():
     case 0:
         expr = view.get_expr()
-        res = rational.parse_all(expr)
+        expr = expr.replace(" ", "")
+        if expr.find("j") < 0:
+            res = rational.parse_all(expr)
+        else:
+            res = complex.re_parse(expr)
         logger.write(f"Результат вычисления: {expr} = {res}")
         view.show(f"Результат вычисления: {expr} = {res}")
     case 1:
         view.show(logger.read_all())
     case 2:
-        view.show("Логи очищены!") if logger.clear() else view.show("Логов нет!")
+        view.show("Логи очищены!") if logger.clear(
+        ) else view.show("Логов нет!")
